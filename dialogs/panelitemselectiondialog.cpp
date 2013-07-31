7b509179fbde3abe71090f0f5395b40f209a4780
dankrusi <dan.krusi@nerves.ch>
2013-07-31 09:22:33 -0500

    Add item dialog previews better supported.

--- dialogs/panelitemselectiondialog.cpp
+++ dialogs/panelitemselectiondialog.cpp
@@ -42,15 +42,21 @@ void PanelItemSelectionDialog::tickTime(double dt, int total) {
 }
 
 void PanelItemSelectionDialog::itemChanged(QListWidgetItem *newItem) {
+    // Remove the previously showed one
     if(selectedPanelItem) {
         scene.removeItem(selectedPanelItem);
         selectedPanelItem->deleteLater();
         selectedPanelItem = 0;
     }
     if(!newItem) return;
+    // Create new preview item
     selectedPanelItem = factory.itemForName(newItem->text(), panel, &simulatedConnection);
     Q_ASSERT(selectedPanelItem);
     Q_ASSERT(ui->itemPreview->scene());
+    selectedPanelItem->setAntialiasEnabled(true); // Wouldn't want the preview to look ugly would we?
     scene.addItem(selectedPanelItem);
+    // Set size - first we get the size we need from the scene's fitInView, then we update the size so that the item can cache any pixmaps it needs, then we refit so that it is centered
+    //TODO: for some reason this is still upscaling any pixmaps within the item...?
     ui->itemPreview->fitInView(selectedPanelItem, Qt::KeepAspectRatio);
+    selectedPanelItem->setSize(selectedPanelItem->width(),selectedPanelItem->height());
 }
